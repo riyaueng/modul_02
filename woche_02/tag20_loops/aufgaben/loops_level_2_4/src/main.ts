1579c88d39dea72676f5b30ef961a201be3bc4bd
riya <riya.ueng@gmail.com>
2025-08-04 08:26:43 +0200

    tag20_bis_aufgabe_3_6

--- woche_02/tag20_loops/aufgaben/loops_level_2_4/src/main.ts
+++ woche_02/tag20_loops/aufgaben/loops_level_2_4/src/main.ts
@@ -1,5 +1,11 @@
-import "./style.css"
+function addToFifty(): void {
+  let sum = 0
+  while (sum <= 50) {
+    const randomNumber: number = Math.floor(Math.random() * 10) + 1
+    console.log(randomNumber)
+    sum += randomNumber
+  }
+  console.log(`${sum} > 50. Stopping right here.`)
+}
 
-// function addToFifty(): void {
-
-// }
+addToFifty()
---
+++ woche_02/tag20_loops/aufgaben/loops_level_3_6/src/main.ts
@@ -0,0 +1,28 @@
+import "./style.css"
+
+function calcAverage(classNumbers: number[]) {
+  if (classNumbers.length === 0) return "Keine Schulnote"
+
+  let averageNumber = classNumbers.reduce((savedValue, currentValue) => savedValue + currentValue, 0)
+  let averageResult = averageNumber / classNumbers.length
+
+  if (averageResult < 50) {
+    console.log(`${averageResult}: Ungenügend`)
+  } else if (averageResult < 60) {
+    console.log(`${averageResult}: Mangelhaft`)
+  } else if (averageResult < 70) {
+    console.log(`${averageResult}: Ausreichend`)
+  } else if (averageResult < 80) {
+    console.log(`${averageResult}: Befriedigend`)
+  } else if (averageResult < 90) {
+    console.log(`${averageResult}: Gut`)
+  } else {
+    console.log(`${averageResult}: Sehr gut`)
+  }
+}
+
+console.log(calcAverage([44, 11, 70, 80, 52, 88, 94, 3, 25, 46]))
+console.log(calcAverage([78, 83, 55, 65, 65, 92, 99, 39, 75, 33]))
+console.log(calcAverage([78, 83, 90, 80, 65, 92, 99, 59, 75, 33]))
+console.log(calcAverage([78, 83, 90, 80, 99, 92, 99, 69, 75, 45]))
+console.log(calcAverage([95, 93, 90, 80, 99, 92, 99, 88, 89, 79]))
