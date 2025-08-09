f22ff048177e57e96ead886d9ee22c803d859b8a
riya <riya.ueng@gmail.com>
2025-08-09 16:35:12 +0200

    tag24_bonusaufgabe_fertig

--- woche_03/tag24_oop_02/aufgaben/oop_class_level_1_12/src/main.ts
+++ woche_03/tag24_oop_02/aufgaben/oop_class_level_1_12/src/main.ts
@@ -4,13 +4,13 @@ import { IceCreamFlavor } from "./classes/Ice"
 const icecream1 = new IceCreamFlavor("Chocolate", 2, true)
 console.log(icecream1)
 
-const icecream2 = new IceCreamFlavor("Mango", 4, true)
+const icecream2 = new IceCreamFlavor("Stracciatella", 3.5, false, "Classic Italian")
 console.log(icecream2)
 
-const icecream3 = new IceCreamFlavor("Stracciatella", 3, false, "Classic Italian")
+const icecream3 = new IceCreamFlavor("Mango", 4.5, true)
 console.log(icecream3)
 
-const icecream4 = new IceCreamFlavor("Mint", 2, false, "Fresh taste")
+const icecream4 = new IceCreamFlavor("Mint", 2.5, false, "Fresh taste")
 console.log(icecream4)
 
 const icecreamArray: IceCreamFlavor[] = [icecream1, icecream2, icecream3, icecream4]
@@ -33,25 +33,86 @@ icecreamArray.forEach((icecream: IceCreamFlavor) => {
 
 // * -------- Aufgabe Class Level 1_12 Bonus --------
 
-const iceContainer1 = document?.querySelector<HTMLDivElement>(".div_icecream")
+const iceContainer1 = document?.querySelector(".div_icecream") as HTMLDivElement
 
-// icecreamArray.forEach((ice: IceCreamFlavor) => {
-//   const divElement = document.createElement("div") as HTMLDivElement
-//   const headlineElement = document.createElement("h2") as HTMLHeadingElement
-//   const textElement = document.createElement("p") as HTMLParagraphElement
-//   headlineElement.textContent = ice._name
-//   headlineElement.style.color = #FF368D
-//   // textElement.textContent = ice._price
-//   textElement.style.color = "black"
-//   divElement.style.backgroundColor = "white"
-//   iceContainer1.appendChild(divElement)
-// })
+icecreamArray.forEach((ice: IceCreamFlavor) => {
+  const divElement = document.createElement("div") as HTMLDivElement
+  const starIcon = document.createElement("p") as HTMLParagraphElement
+  const headlineElement = document.createElement("h2") as HTMLHeadingElement
+  const priceElement = document.createElement("p") as HTMLParagraphElement
+  const scoopsElement = document.createElement("p") as HTMLParagraphElement
+  const totalPriceElement = document.createElement("p") as HTMLParagraphElement
+  const numberBtn = document.createElement("button") as HTMLButtonElement
+  const placeholder = document.createElement("div") as HTMLDivElement
 
-// obstsalat.forEach((obst: string) => {
-//   const listElement = document.createElement("li") as HTMLLIElement
+  // ----- Styling der Boxen -----
+  iceContainer1.style.display = "flex"
+  iceContainer1.style.justifyContent = "center"
+  iceContainer1.style.alignItems = "Center"
 
-//   listElement.style.color = "red"
-//   listElement.style.listStyle = "none"
-//   listElement.textContent = obst
-//   ungeordneteListeElement.appendChild(listElement)
-// })
+  divElement.style.textAlign = "center"
+  divElement.style.display = "flex"
+  divElement.style.flexDirection = "column"
+  divElement.style.justifyContent = "center"
+  divElement.style.alignItems = "center"
+  divElement.style.backgroundColor = "white"
+  divElement.style.width = "14rem"
+  divElement.style.margin = "2rem"
+  divElement.style.borderRadius = "0.5rem"
+  divElement.style.boxShadow = "0px 0px 5px rgba(0,0,0,0.3)"
+  divElement.style.color = "black"
+  divElement.style.fontSize = "0.8rem"
+
+  starIcon.textContent = "⭐️"
+  starIcon.style.fontSize = "1.5rem"
+  starIcon.style.marginTop = "1rem"
+  // ---- Placeholder-Element für Icon ----
+  placeholder.style.height = "2.3rem"
+  placeholder.style.width = "1.5rem"
+
+  placeholder.style.marginTop = "1rem"
+
+  headlineElement.textContent = ice._name
+  headlineElement.style.fontSize = "1.5rem"
+  headlineElement.style.color = "#FF368D"
+
+  priceElement.textContent = String(`Price: ${ice._price} Euro`)
+  scoopsElement.textContent = "0"
+  totalPriceElement.textContent = "0 Euro"
+
+  numberBtn.textContent = "+1"
+  numberBtn.style.color = "white"
+  numberBtn.style.backgroundColor = "#FF368D"
+  numberBtn.style.width = "2rem"
+  numberBtn.style.height = "2rem"
+  numberBtn.style.border = "none"
+  numberBtn.style.borderRadius = "0.2rem"
+  numberBtn.style.margin = "1rem"
+
+  // ----- Styling-Elemente übergeben in HTML -----
+
+  iceContainer1.appendChild(divElement)
+  // ? --- Beliebtes Eissorten kennzeichnen ---
+  if (ice._isPopular) {
+    divElement.appendChild(starIcon)
+  } else {
+    divElement.appendChild(placeholder)
+  }
+  divElement.appendChild(headlineElement)
+  divElement.appendChild(priceElement)
+  divElement.appendChild(scoopsElement)
+  divElement.appendChild(totalPriceElement)
+  divElement.appendChild(numberBtn)
+
+  // ----- Anzahl der Kugeln eingeben -----
+  let scoopsCount: number = 0
+
+  numberBtn?.addEventListener("click", () => {
+    if (numberBtn) {
+      scoopsCount++
+      scoopsElement.textContent = String(scoopsCount)
+      let iceTotalprice = ice.getTotalPrice(scoopsCount)
+      totalPriceElement.textContent = `${iceTotalprice} Euro`
+    }
+  })
+})
