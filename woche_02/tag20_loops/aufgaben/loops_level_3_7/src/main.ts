468abb459551c92f7d16fa7ec6ea2543950e7d74
riya <riya.ueng@gmail.com>
2025-08-27 15:37:55 +0200

    tag20_loop_bonus_aufgabe_fertig

--- woche_02/tag20_loops/aufgaben/loops_level_3_7/src/main.ts
+++ woche_02/tag20_loops/aufgaben/loops_level_3_7/src/main.ts
@@ -1,5 +1,49 @@
-function encryptRot13(letterString: string): void {
-  const alphabetArray: string[] = []
-  const alphabetLetters = "abcdefghijklmnopqrstuvwxyz"
-  const upperLetter = letterString.toUpperCase()
+console.log("Encrypted message with Rot13.")
+
+const alphabetLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+]
+console.log(alphabetLetters)
+
+function encryptRot13(word: string): string {
+  let result = ""
+
+  for (let i = 0; i < word.length; i++) {
+    const upperLetter = word[i].toUpperCase()
+    const alphabetIndex = alphabetLetters.indexOf(upperLetter)
+    const indexRot13 = (alphabetIndex + 13) % alphabetLetters.length
+    result = result + alphabetLetters[indexRot13]
+
+    if (upperLetter === " ") {
+      result = result + " "
+    }
+  }
+  return result
 }
+
+console.log(encryptRot13("Encrypted message with Rot13."))
